f3f8875e6b2da215f922339eefdcaef864e526ca
Alexiatr <himmatovdilshod06@gmail.com>
2024-12-28 21:01:28 +0500

    one plus task for today

---
+++ codes/for/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=10, sum=0;
+
+    for (int i=0; i<=n; i++) {
+        sum=sum+i;
+    }
+    cout<< "yig'indi: "<<sum<<endl;
+    return 0;
+}
