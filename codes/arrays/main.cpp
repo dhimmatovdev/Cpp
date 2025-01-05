106b73397901769ef85be9dcc8ff3898e42c5558
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-05 22:54:03 +0500

    array qo'shildi

---
+++ codes/arrays/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 5 o'lchamli massive
+    int myarrays[5]={12,23,34,45,56};
+    cout<<sizeof(myarrays)<<endl;
+    // Nima uchun 20 natija chiqaradi? 
+    // Chunki sizeof operatori byte larda qiymat qaytaradi
+    // 4 byte * 5 = 20 byte int tipi 4 byte
+    return 0;
+}
