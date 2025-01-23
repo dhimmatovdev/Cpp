108190f465ce57c705dcb6fc21f7581846e9edf9
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-23 23:34:49 +0500

    xato qilayabman

--- dasturlash_uz/array/arr_3.cpp
+++ dasturlash_uz/array/arr_3.cpp
@@ -11,5 +11,11 @@ int main() {
     int num[]={6, 25, 91, 23, 72, 9, 18, 6};
     int size = sizeof(num)/sizeof(num[0]);
     int sum=0;
-    for (int)
+    int sum1, result1;
+    for (int i=0; i<size; i++) {
+        sum++;
+        int sum1=sum+num[i];
+        int result1=sum1/sum;
+    }
+    cout << result1 <<endl;
 }
