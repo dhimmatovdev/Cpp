f12e97564d82aed0e1dcf51dab3982f6dbff8ee8
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-23 23:27:55 +0500

    array boshlandi uchunchi o'ji

--- dasturlash_uz/array/arr_3.cpp
+++ dasturlash_uz/array/arr_3.cpp
@@ -4,3 +4,12 @@ Massive (array) berilgan.
 
 Massive elementlarini o'rtacha qiymatlarini hisoblang va konsolga chiqaring.
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num[]={6, 25, 91, 23, 72, 9, 18, 6};
+    int size = sizeof(num)/sizeof(num[0]);
+    int sum=0;
+    for (int)
+}
