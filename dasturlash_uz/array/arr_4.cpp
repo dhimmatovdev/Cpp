3a016588d2f255e4120b4baefdbfac78c7112bd6
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-27 22:39:37 +0500

    arraydan

---
+++ dasturlash_uz/array/arr_4.cpp
@@ -0,0 +1,14 @@
+/*
+Misol Sharti
+n, array[] berilgan. 
+
+Shu massivda n soni bor yoki  yo'qligini aniqlang. n soni bo'lsa true, bo'lmasa false konsolga chiqaring.
+
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[]={6, 25, 91, 23, 72, 9, 18, 6};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    
+}
