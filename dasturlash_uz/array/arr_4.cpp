feeb0b9c21904ac80277f36b6d340aa6145a434d
Dilshod Himmatov <himmatovdilshod06@gmail.com>
2025-02-04 18:58:03 +0500

    arraydan

--- dasturlash_uz/array/arr_4.cpp
+++ dasturlash_uz/array/arr_4.cpp
@@ -10,5 +10,15 @@ using namespace std;
 int main() {
     int arr[]={6, 25, 91, 23, 72, 9, 18, 6};
     int size = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cin>>n;
+    bool found = false;
+    for(int i = 0; i<size; i++) {
+        if(arr[i]==n) {
+            found = true;
+            break;
+        }
+    }
+    cout<<found<<endl;
     
 }
