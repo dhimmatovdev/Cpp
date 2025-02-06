4ad37b99174ff2289848bebabe152c11cecbaa24
Alexiatr <himmatovdilshod06@gmail.com>
2025-02-07 00:49:04 +0500

    code

--- dasturlash_uz/array/arr_6.cpp
+++ dasturlash_uz/array/arr_6.cpp
@@ -7,18 +7,25 @@ Shu massiv oxiriga n ni jo'ylashtiring va massivni konsolga chiqaring.
 
     */
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
-    int arr[]={6, 25, 91, 23, 72, 9, 18, 6};
-    int size=sizeof(arr)/sizeof(arr[0]);
+    vector<int> arr = {6, 25, 91, 23, 72, 9, 18, 6};
     int n;
-    cin>>n;
-    arr[size]=n;
-    for (int i =0; i<size+1; i++) {
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    cin >> n;
     
+    // `push_back` orqali yangi element qo‘shamiz
+    arr.push_back(n);
+    
+    // Massivni chiqarish
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
+// vektor avtomatik o'lchamni o'zgartira oladi.
 
