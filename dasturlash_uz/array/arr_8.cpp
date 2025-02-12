f63a3cc4ac4ff014a7e0a1ec98ccf27a40f35b7c
Alexiatr <himmatovdilshod06@gmail.com>
2025-02-12 11:58:00 +0500

    arayydan

--- dasturlash_uz/array/arr_8.cpp
+++ dasturlash_uz/array/arr_8.cpp
@@ -5,6 +5,9 @@ int main() {
     int arr[]={1,3,7,8,4};
     int size=sizeof(arr)/sizeof(arr[0]);
     int NewArr[size];
-    cout<<NewArr<<endl;
+    for (int i = 0; i<size; i++) {
+        cout<<arr[i];
+    }
+    //cout<<NewArr<<endl;
     return 0;
 }
