e0d0a9979fbfb0c7865897dc595bea79e21604c5
Alexiatr <himmatovdilshod06@gmail.com>
2025-02-12 12:01:41 +0500

    arayydan

--- dasturlash_uz/array/arr_8.cpp
+++ dasturlash_uz/array/arr_8.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
     int arr[]={1,3,7,8,4};
     int size=sizeof(arr)/sizeof(arr[0]);
-    int NewArr[size];
     for (int i = 0; i<size; i++) {
-        cout<<arr[i];
+        int NewArr[]={arr[i]};
+        cout<<NewArr[i];
     }
+    cout<<endl;
     //cout<<NewArr<<endl;
     return 0;
 }
